ad0d71fd91dfa6ba817c150122e405a2ab9baff4
James Buchanan <jbuchanan@newrelic.com>
2025-11-03 14:27:55 +0000

    added custom threshold support to regions

--- visualizations/geo-location-viz/components/Regions.tsx
+++ visualizations/geo-location-viz/components/Regions.tsx
@@ -2,18 +2,27 @@ import React, { useEffect } from "react";
 
 import { generateTooltipConfig } from "../utils";
 import { useProps } from "../context/VizPropsProvider";
-import { useNerdGraphQuery } from "../hooks/useNerdGraphQuery";
+import { useDualQuery } from "../hooks/useNerdGraphQuery";
 import { useHeatmap } from "../hooks/useHeatmap";
 
 import Region from "./Region";
 
 const Regions = () => {
-  const { regionsQuery } = useProps();
+  const { 
+    regionsQuery, 
+    thresholdQuery,
+    thresholdMatchField = 'name'
+  } = useProps();
+  
   if (regionsQuery === null || regionsQuery === undefined) {
     return null;
   }
 
-  const { data: regions } = useNerdGraphQuery(regionsQuery);
+  const { data: regions } = useDualQuery(
+    regionsQuery, 
+    thresholdQuery, 
+    thresholdMatchField
+  );
 
   const { setRange, heatMapSteps, getGradientColor } = useHeatmap();
   useEffect(() => {
